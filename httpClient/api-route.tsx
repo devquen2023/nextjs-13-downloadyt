60ae16c5415621076e147bc30f0797051b2e909e
thind9x <developer@phuongmychi.vn>
2023-10-19 18:02:55 +0700

    new

---
+++ httpClient/api-route.tsx
@@ -0,0 +1,4 @@
+// export const API_URL = "https://api.phuongmychi.vn/v2";
+// export const API_URL_TOKEN = "https://api.phuongmychi.vn";
+    export const API_URL = "http://localhost:8080/v2";
+    export const API_URL_TOKEN = "http://localhost:8080";
